537ceedd3a3af0ffb997fb11bf4fc60315772bdc
subham <bsubham874@gmail.com>
2022-11-11 09:29:59 +0000

    Update airdrop.js

--- airdrop.js
+++ airdrop.js
@@ -2,35 +2,36 @@
 // npm install --save @solana/web3.js
 
 const { 
-  Connection, 
-  LAMPORTS_PER_SOL, 
-  clusterApiUrl, 
-  Keypair
-} = require("@solana/web3.js");
-
-const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
-
-(async ()=> {
-
-  const keypair = Keypair.generate();
-
-  const airdropSignature = await connection.requestAirdrop(
-    keypair.publicKey,
-    LAMPORTS_PER_SOL
-  );
+    Connection, 
+    LAMPORTS_PER_SOL, 
+    clusterApiUrl, 
+    Keypair
+  } = require("@solana/web3.js");
   
-  const latestBlockHash = await connection.getLatestBlockhash();
+  const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
   
-  const txn = await connection.confirmTransaction({
-    blockhash: latestBlockHash.blockhash,
-    lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
-    signature: airdropSignature,
-  });
-
-  console.log({
-    publicKey: keypair.publicKey,
-    privateKey: keypair.secretKey,
-    signature: airdropSignature,
-    txn
-  })
-})()
+  (async ()=> {
+  
+    const keypair = Keypair.generate();
+  
+    const airdropSignature = await connection.requestAirdrop(
+      keypair.publicKey,
+      LAMPORTS_PER_SOL
+    );
+    
+    const latestBlockHash = await connection.getLatestBlockhash();
+    
+    const txn = await connection.confirmTransaction({
+      blockhash: latestBlockHash.blockhash,
+      lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
+      signature: airdropSignature,
+    });
+  
+    console.log({
+      publicKey: keypair.publicKey,
+      privateKey: keypair.secretKey,
+      signature: airdropSignature,
+      txn
+    })
+  })()
+  
